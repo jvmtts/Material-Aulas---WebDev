4f41ca3cb51cd57cd39cf51bc740113d086000d3
João <joaovictorrmattos@gmail.com>
2025-09-11 22:44:24 -0300

    APIS

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
+
 export default function Usuarios() {
+  useEffect(() => {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(erro => console.log(erro))
+  .finally(console.log("Carregou desgraça"))
+  },[])
   return (
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold">Usuários</h1>
-      <p className="mt-2 text-gray-600">Página de usuários.</p>
     </div>
   );
 }
